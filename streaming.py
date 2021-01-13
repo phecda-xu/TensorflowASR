138fb5ee507a4675a53ff4c50c06a5af0d7bcb12
phecda-xu <viaxu910515@163.com>
2021-01-13 15:10:09 +0800

    update

--- streaming.py
+++ streaming.py
@@ -20,7 +20,34 @@ class StreamingASR(object):
         self.decoded = tf.constant([self.text_featurizer.start])
 
     def stream_detect(self, inputs):
-        pass
+        data = self.speech_feature.load_wav(inputs)
+
+        if self.am.model.mel_layer is None:
+            mel = self.speech_feature.extract(data)
+            x = np.expand_dims(mel, 0)
+        else:
+            mel = data.reshape([1, -1, 1])
+            x = self.am.model.mel_layer(mel)
+        x = self.am.model.encoder(x)   # TensorShape([1, 109, 144])
+        step = x.shape[1]
+        i = 0
+        while i < step:
+            self.step_decode(x[:, i])
+            i = i+1
+
+    def step_decode(self, step_input):
+        enc = tf.reshape(step_input, [1, 1, -1])
+        y = self.am.model.predict_net(inputs=tf.reshape(self.decoded, [1, -1]),
+                                      p_memory_states=None,
+                                      training=False)
+        y = y[:, -1:]
+        z = self.am.model.joint_net([enc, y], training=False)
+        probs = tf.squeeze(tf.nn.log_softmax(z))
+        pred = tf.argmax(probs, axis=-1, output_type=tf.int32)
+        pred = tf.reshape(pred, [1])
+        if pred != 0 and pred != self.text_featurizer.blank:
+            self.decoded = tf.concat([self.decoded, pred], axis=0)
+            print("pred: {}".format(self.text_featurizer.index_to_token[pred.numpy().tolist()[0]]))
 
     def predict_stack_buffer(self, wavfile):
         data = self.speech_feature.load_wav(wavfile)
@@ -58,41 +85,12 @@ class StreamingASR(object):
             j += 1
         print(1)
 
-    def predict(self, wavfile):
-        data = self.speech_feature.load_wav(wavfile)
-
-        if self.am.model.mel_layer is None:
-            mel = self.speech_feature.extract(data)
-            x = np.expand_dims(mel, 0)
-        else:
-            mel = data.reshape([1, -1, 1])
-            x = self.am.model.mel_layer(mel)
-        x = self.am.model.encoder(x)
-        step = x.shape[1]
-        i = 0
-        while i < step:
-            enc = tf.reshape(x[:, i], [1, 1, -1])
-            y = self.am.model.predict_net(inputs=tf.reshape(self.decoded, [1, -1]),
-                                          p_memory_states=None,
-                                          training=False)
-            y = y[:, -1:]
-            z = self.am.model.joint_net([enc, y], training=False)
-            logits = tf.squeeze(tf.nn.log_softmax(z))
-            pred = tf.argmax(logits, axis=-1, output_type=tf.int32)
-            pred = tf.reshape(pred, [1])
-            if pred != 0 and pred != self.text_featurizer.blank:
-                self.decoded = tf.concat([self.decoded, pred], axis=0)
-                print("step: {}, "
-                      "pred: {}".format(i,
-                                        self.text_featurizer.index_to_token[pred.numpy().tolist()[0]]))
-            i += 1
-
-        print(1)
 
 if __name__ == "__main__":
     am_config = UserConfig(r'./pre_train/rnnt/am_data.yml', r'./pre_train/rnnt/conformer.yml')
     # am_config = UserConfig(r'./configs/am_data_back.yml', r'./configs/tdnn.yml')
     model = StreamingASR(am_config)
-    model.predict('BAC009S0002W0122.wav')
+    model.stream_detect('CppInference/test.wav')
+
 
 
